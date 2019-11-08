1c07b060d6c303f60507d8772a7e0a614a159429
pelzolga123 <pelzolga123@gmail.com>
2019-11-08 18:40:20 +0200

    Added turns function

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,7 @@
+var turn = "X";
+var moves = 9;
+var array = [];
+
 const gameBoard = () => {
     const mainDiv = document.getElementById('game');
     const table = document.createElement('table');
@@ -20,18 +24,32 @@ const gameBoard = () => {
     mainDiv.appendChild(table);
 };
 
+ function turns() {
+    moves--; 
+    
+    if (this.innerHTML !== "") {
+        return;
+    }
+    this.innerHTML = turn;
+
+    if(turn === "X") {
+        turn = "O"
+    }
+    else{
+        turn = "X"
+    }
+    array.push(turn);
+    console.log(turn);       
+    console.log(moves);
+    console.log(array);    
+    
+    
+}
 
 const clicked = id => {
     document.addEventListener('DOMContentLoaded',function(){
-    const cellId = document.getElementById(id);
-    if(cellId.innerHTML == ""){
-    cellId.addEventListener('click', function(){   
-        cellId.innerHTML = id;
-    });
-    }
-    else{
-        console.log('not working');
-    }
+        const cellId = document.getElementById(id);    
+        cellId.addEventListener('click', turns)
     })
 };
  

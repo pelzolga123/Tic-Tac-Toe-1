c6a67f0df6cf5008f042e42f183c1a726d346737
tundeiness <tundeiness@yahoo.co.uk>
2019-11-11 17:08:08 +0100

    add right parentheses

--- javascript/player.js
+++ javascript/player.js
@@ -6,7 +6,7 @@ const playerFactory = (name, symbol) => {
 
 
   const setSymbol = (playerOne) => {
-    if (playerOne.getSymbol() {
+    if (playerOne.getSymbol()) {
       console.log(`${playerOne.getSymbol()}`);
     } else {
       console.log(`${playerTwo.getSymbol()}`);
